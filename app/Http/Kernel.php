b40ea0f36a6bf4d1543ee931036862a3e9c90413
moassaad <mohammadasaadgo@gmail.com>
2024-11-03 18:28:52 +0200

    update: register guest api middleware and set api language.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\ApiLangMiddleware::class,
         ],
     ];
 
@@ -65,5 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        
+        'guest.api' => \App\Http\Middleware\HandleIfAuthenticated::class,
     ];
 }
