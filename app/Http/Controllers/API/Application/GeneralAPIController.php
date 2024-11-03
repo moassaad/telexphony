3275cb269d5074be914175304a289837325de540
moassaad <mohammadasaadgo@gmail.com>
2024-11-03 18:33:25 +0200

    create: app general controller.

---
+++ app/Http/Controllers/API/Application/GeneralAPIController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\Application;
+
+
+use App\Http\Controllers\Controller;
+
+class GeneralAPIController extends Controller
+{
+    public function test()
+    {
+        // test code...
+    }
+}
